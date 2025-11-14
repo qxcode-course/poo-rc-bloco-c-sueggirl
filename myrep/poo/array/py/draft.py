24b9b30ea37769345dbcaaf53c285ccb861826e0
sueggirl <heysuegirl@gmail.com>
2025-11-14 17:25:04 -0300

    terminar dps de ver video do sena

--- myrep/poo/array/py/draft.py
+++ myrep/poo/array/py/draft.py
@@ -1,15 +1,3 @@
 class Foo:
-    def __init__(self, x: int):
-        self.x = x
-
-    def __str__(self):
-        return f'Foo({self.x})'
-
-lista_vazia: list[int] = []
-lista_preenchida: list[int] = [1, 2, 3, 4, 5]
-lista_preencida_objetos: list[Foo] = [Foo(1), Foo(2), Foo(3), Foo(4), Foo(5)]
-
-lista_vazia.append(1)
-lista_preenchida.append(Foo(6))
-
-
+    def __init__(self, valor):
+        self.valor = valor
